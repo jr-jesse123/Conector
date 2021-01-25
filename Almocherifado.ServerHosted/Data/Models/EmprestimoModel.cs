53028349d2ba5ea365625f908a79229483fad099
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-01-25 05:24:14 -0300

    Criação do EmprestimoModel

---
+++ Almocherifado.ServerHosted/Data/Models/EmprestimoModel.cs
@@ -0,0 +1,19 @@
+﻿using Almocherifado.core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Almocherifado.ServerHosted.Data.Models
+{
+    public class EmprestimoModel
+    {
+        public DateTime Entrega { get; set; }
+        public FuncionarioModel Funcionario { get; set; }
+        public List<Ferramenta> Ferramentas { get; set; }
+        public string   Obra { get; set; }
+    }
+
+    //TODO: CONTINUAR CRIANDO A VALIDAÇÃO DE EMPRÉSTIMO´E O FORMULÁRIO PARA ADICIONAR NOVO EMPRESTIMO
+
+}
