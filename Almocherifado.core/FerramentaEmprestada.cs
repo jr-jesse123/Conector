26c0e888013f3e2d95691096a8c2e53ce9e33737
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-01-25 05:23:30 -0300

    inclusão de propriedade para identificar se a ferramenta já foi recebida na classe FerramentaEmprestada

--- Almocherifado.core/FerramentaEmprestada.cs
+++ Almocherifado.core/FerramentaEmprestada.cs
@@ -18,9 +18,16 @@ namespace Almocherifado.core
             DataDevolucao = DateTime.Now;
         }
 
+
+        public bool Recebido 
+        {
+            get => DataDevolucao is null ? false : true;
+        }
+
         public int Id { get; }
         public virtual Emprestimo Emprestimo { get; set; }
         public DateTime? DataDevolucao { get; private set; }
         public virtual Ferramenta Ferramenta { get; }
+
     }
 }
