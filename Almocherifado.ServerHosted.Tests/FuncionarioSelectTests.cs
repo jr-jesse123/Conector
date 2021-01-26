430b1b3c1053a1b59e191727ae89e523828a59ce
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-01-26 20:55:40 -0300

    Revert "códigos de exemplos retirados"
    This reverts commit 3e0c3d0950a535bd2de6e1e5d1d5fd2ed0f0f4a3.

--- Almocherifado.ServerHosted.Tests/FuncionarioSelectTests.cs
+++ Almocherifado.ServerHosted.Tests/FuncionarioSelectTests.cs
@@ -33,13 +33,42 @@ namespace Almocherifado.ServerHosted.Tests
             this.testOutputHelper = testOutputHelper;
         }
 
+        public string MyProperty { get; set; } = "";
 
         [Theory, DomainAutoData]
         public void Mudancas_Do_Cpf_Sao_Refletidas_No_CallBakc(List<Funcionario> funcionarios)
         {
-            string MyProperty = "";
+            //Mock<Action<string>> mock = new Mock<Action<string>>();
+            var Mock = "";
 
-             var ParentTitle = ComponentParameterFactory.Parameter(nameof(FuncionarioSelect.ParentsCPF), MyProperty);
+            EventCallback<string> callback
+                = new EventCallback<string>();
+
+
+            var OnCpfescolhido = ComponentParameterFactory.Parameter("ParentsCPF", MyProperty);
+            var Onchanged = ComponentParameterFactory.Parameter("ParentsCPFChanged", callback);
+            var funcionariosParameter = ComponentParameterFactory.Parameter("FuncionariosSource", funcionarios);
+
+            var cut = RenderComponent<FuncionarioSelect>(OnCpfescolhido, Onchanged, funcionariosParameter);
+
+            cut.FindComponent<Select<string>>().Find("Select")
+                .Change(new ChangeEventArgs() 
+                {
+                    Value = funcionarios[2].CPF
+                });
+
+
+            //cut.FindComponent<Select<string>>().Instance.SelectedValue.Should().Be(funcionarios[2].CPF);
+
+            MyProperty.Should().Be(funcionarios[2].CPF);
+
+            //mock.Verify(a => a.Invoke(It.IsAny<ChangeEventArgs>()), Times.Once);
+        }
+
+        [Theory, DomainAutoData]
+        void testeBind(List<Funcionario> funcionarios)
+        {
+            var ParentTitle = ComponentParameterFactory.Parameter(nameof(FuncionarioSelect.ParentsCPF), MyProperty);
 
             var callback = ComponentParameterFactory.EventCallback<string>(nameof(FuncionarioSelect.ParentsCPFChanged),
                 args => MyProperty = args.ToString());
@@ -48,8 +77,36 @@ namespace Almocherifado.ServerHosted.Tests
 
             cut.Find("select").Change(new ChangeEventArgs() { Value = funcionarios[2].CPF });
 
+            //var cut = RenderComponent<FuncionarioSelect>(
+            //builder =>
+            //{
+            //    builder.Add(c => c.ParentsCPF, MyProperty);
+            //    builder.Add(c => c.ParentsCPFChanged, (e) =>
+            //    {
+            //        textChangedEventTriggered = e;
+            //    }) );
+            //});
+
+
+
+
             MyProperty.Should().Be(funcionarios[2].CPF.ToString());
 
+        }
+
+        void teste2()
+        {
+
+        }
+
+      
+
+        class teste : IHandleEvent
+        {
+            public Task HandleEventAsync(EventCallbackWorkItem item, object arg)
+            {
+                throw new NotImplementedException();
+            }
         }
 
     }
